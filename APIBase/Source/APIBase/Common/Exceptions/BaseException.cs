382892158f08b668dcaf2eccf2354fbf8435d176
christofer <chrisb66110@gmail.com>
2020-06-12 18:25:41 -0600

    Add BaseException

---
+++ APIBase/Source/APIBase/Common/Exceptions/BaseException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+
+namespace APIBase.Common.Exceptions
+{
+    public abstract class BaseException : Exception
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        
+        protected BaseException(
+            string message)
+            :base(message)
+        {
+        }
+
+        protected BaseException(
+            string message,
+            HttpStatusCode statusCode)
+            : base(message)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
