f3e50819a5b4291e75c8a377d3597b53dc2431e3
christofer <chrisb66110@gmail.com>
2020-06-15 21:06:36 -0600

    Add BaseSettings with AuthenticationSetting

---
+++ APIBase/Source/APIBase/Common/Settings/BaseAuthenticationSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace APIBase.Common.Settings
+{
+    [ExcludeFromCodeCoverage]
+    public class BaseAuthenticationSettings
+    {
+        public string Authority { get; set; }
+        public string Audience { get; set; }
+    }
+}
