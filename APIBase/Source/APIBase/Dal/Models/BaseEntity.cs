85f721a177756be49af68cb7e76ae7a1acdb2843
christofer <chrisb66110@gmail.com>
2020-07-05 11:42:49 -0600

    Add BaseEntity

---
+++ APIBase/Source/APIBase/Dal/Models/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace APIBase.Dal.Models
+{
+    [ExcludeFromCodeCoverage]
+    public class BaseEntity<T>
+        where T : struct
+    {
+        [Key]
+        public T Id { get; set; }
+    }
+}
